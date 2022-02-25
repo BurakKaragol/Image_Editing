d1f508848b0f777dd64e0c836ee09a031c0d1905
Burak Karagol <34738016+BurakKaragol@users.noreply.github.com>
2022-02-25 17:25:57 +0300

    Circle Packing Algorithm
    adding shape control using text and commenting lines

--- CirclePacking.py
+++ CirclePacking.py
@@ -3,29 +3,44 @@ from random import randint, choice
 import cv2 as cv
 import numpy as np
 
+#Editable
 img_w = 800
 img_h = 600
 background_color = (0, 0, 0)
 draw_color = (255, 255, 255)
 circle_thickness = 2
-img = np.zeros((img_h, img_w, 3), dtype=np.uint8)
-img[0:img_h, 0:img_w] = background_color
-example_img = cv.imread('image.png')
-example_img = cv.resize(example_img, (img_w, img_h))
+text = "BURAK"
+font_size = 5
+text_multiplier_x = 25
+text_multiplier_y = 15
+text_thickness = 15
+total_val = 10
+max_attempts = 100
+useText = True
+fillCircle = False
+auto_restart = False
+
+#Non Editable
 circles = []
 available = []
+img = np.zeros((img_h, img_w, 3), dtype=np.uint8)
+img[0:img_h, 0:img_w] = background_color
+text_img = np.zeros((img_h, img_w, 3), dtype=np.uint8)
+text_img[0:img_h, 0:img_w] = background_color
+text_pos = ((img_w // 2) - len(text) * text_multiplier_x * (font_size // 2), (img_h // 2) + text_multiplier_y * (font_size // 2))
+print(text_pos)
+text_img = cv.putText(text_img, text, text_pos, cv.FONT_HERSHEY_SIMPLEX, font_size, (draw_color), text_thickness)
 canDraw = True
-useImg = True
-restart = False
 
-if useImg:
+# calculate teh coorinates that is inside the white area
+if useText:
     for i in range(0, img_h - 1):
         for j in range(0, img_w - 1):
-            if example_img[i, j][0] == 255:
+            if text_img[i, j][0] == 255:
                 available.append((i, j))
+print('calculated : ' + str(len(available)))
 
-print(len(available))
-
+# circle class for keeping the circle positions
 class Circle():
     growing = True
 
@@ -36,30 +51,39 @@ class Circle():
         self.clr = color
 
     def show(self, img):
-        img = cv.circle(img, (self.x, self.y), self.r, self.clr, circle_thickness)
+        if fillCircle:
+            img = cv.circle(img, (self.x, self.y), self.r, self.clr, cv.FILLED)
+        else:
+            img = cv.circle(img, (self.x, self.y), self.r, self.clr, circle_thickness)
 
     def grow(self):
         if self.growing:
             self.r += 1
-
+    
+    # returns if any of the img bounds is exceeded
     def edges(self):
         return self.x + self.r >= img_w or self.x - self.r <= 0 or self.y + self.r >= img_h or self.y - self.r <= 0
 
 def NewCircle():
-    if useImg:
-        r_index = randint(0, len(available))
-        y_pos, x_pos = choice(available)
+    # get random point whether on the text or the image
+    if useText:
+        if len(available) != 0: 
+            y_pos, x_pos = choice(available)
+        else:
+            y_pos, x_pos = 0, 0
     else:
         x_pos = randint(0, img_w)
         y_pos = randint(0, img_h)
-    
+
+    # check if the random point is inside another circle
     valid = True
     for circle in circles:
         d = dist((x_pos, y_pos), (circle.x, circle.y))
         if d < circle.r + (circle_thickness // 2):
             valid = False
             break
-
+    
+    # return the valid circle found
     if valid:
         return Circle(x_pos, y_pos, 1, draw_color)
     else:
@@ -68,20 +92,22 @@ def NewCircle():
 while True:
     img[0:img_h, 0:img_w] = background_color
 
-    total = 50
+    # try to find valid circles untill there are no empty place
+    total = total_val
     count = 0
     attempts = 0
-
     while count < total and canDraw:
         temp_circle = NewCircle()
         if temp_circle is not None:
             circles.append(temp_circle)
             count += 1
         attempts += 1
-        if attempts > 100:
+        if attempts > max_attempts:
             canDraw = False
             print("Cannot create new circles")
+            print('stopped')
 
+    # checking if any of the circle is touching another one     
     for circle in circles:
         if circle.growing:
             if circle.edges():
@@ -89,26 +115,27 @@ while True:
             else:
                 for otherCircle in circles:
                     if circle != otherCircle:
+                        # and if there are one stops the growing
                         d = dist((circle.x, circle.y), (otherCircle.x, otherCircle.y))
                         if d - circle_thickness < circle.r + otherCircle.r:
                             circle.growing = False
+                            otherCircle.growing = False
                             break
         circle.grow()
         circle.show(img)
-
+    
     cv.imshow("Circle Packing Algorithm", img)
 
     c = cv.waitKey(1) % 256
 
-    if c == ord('r') or (not canDraw and restart):
+    if c == ord('r') or (not canDraw and auto_restart):
         circles = []
         canDraw = True
     
-    if c == ord('t'):
-        if useImg:
-            useImg = False
-        else:
-            useImg
+    elif c == ord('s'):
+        temp = 'circle_packing_' + text + '.jpg'
+        cv.imwrite(temp, img)
+        print("saved as " + temp)
 
     if c == ord('q'):
         break
